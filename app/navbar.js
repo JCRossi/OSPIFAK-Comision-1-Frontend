5e84c6cee3e15c9ef8fd7067acbdd886a3c8f70a
Juan Cruz Rossi <juanrossi1969@gmail.com>
2023-10-31 09:38:37 -0300

    Línea sobrante problemática

--- app/navbar.js
+++ app/navbar.js
@@ -1,5 +1,4 @@
 import  "./globals.css";
-import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
