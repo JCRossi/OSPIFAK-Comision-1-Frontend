550a38d8b808c16a27297e032fbe82d467ebd1f4
albanomazzino <albimazzino@gmail.com>
2023-11-07 17:35:01 -0300

    Navbar switch temporal

--- app/navbar.js
+++ app/navbar.js
@@ -1,57 +1,39 @@
-"use client"
+"use client";
+
 import  "./globals.css";
-import React, { useEffect, useState } from 'react';
-import { Navbar, Nav, Container, Badge, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-export default function NavbarPagina() {
-  const [clienteData, setClienteData] = useState({
-    dni: '',
-    nombre: '',
-    apellido: '',
-    fechaNacimiento: '',
-    forma_pago: '',
-  });
 
-  useEffect(() => {
-    // Obtener los datos del cliente almacenados en el localStorage
-    const storedClienteData = JSON.parse(localStorage.getItem('cliente'));
+export default function Navbar() {
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('nombre');
+    window.location.href = "/login";
+  };
+    const usuario = localStorage.getItem('nombre');
+    
 
-    if (storedClienteData) {
-      // Si se encuentran datos en el localStorage, actualiza el estado
-      setClienteData(storedClienteData);
-    }
-  }, []);
-
-  const handleClientProfile = () => {
-    window.location.href = '/profile';
-  }
-
-  return (
-    <>
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <img src="/imagen.jpeg" alt="Imagen" style={{ width: '120px', height: '70px' }} />
-          <Navbar.Toggle aria-controls="navbarNav" />
-          <Navbar.Collapse id="navbarNav">
-            <Nav className="ms-auto">
-              <Nav.Item>
-                  <h2>
-                    <Badge bg="light" text="dark">
-                      Cliente {clienteData.nombre}
-                    </Badge>
-                  </h2>
-              </Nav.Item>
-              <Nav.Item>
-                <Button variant="light" size="lg" onClick={handleClientProfile}>
-                  <FontAwesomeIcon icon={faUser} />
+    return (
+      <>
+        <div className="navbar navbar-expand-md navbar-light navbar shadow-sm">
+            <div className="container" >
+                <a className="navbar-brand" href="/dashboard">
+                    <img src="/imagen.jpeg" alt="Imagen" style={{ width: '120px', height: 'auto' }} />
+                </a>
+                <div className="d-flex align-items-center">
+                  <h6 className="ms-2">{usuario} <FontAwesomeIcon icon={faUser} /> </h6>
+                  <Button variant="outline-success" onClick={handleLogout} className="ms-2">
+                  <FontAwesomeIcon icon={faSignOutAlt} /> 
                 </Button>
-              </Nav.Item>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
-  );
+                </div>
+                
+                
+            </div>
+        </div>
+      </>
+    );
+  
 }
